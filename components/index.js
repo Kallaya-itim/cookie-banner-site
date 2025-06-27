d900aa4b1a9a5d35ea4fee3cfb2e860436ecccfc
Kallaya-itim <kallaya1331@gmail.com>
2025-06-27 14:43:27 +0700

    Add files via upload

---
+++ components/index.js
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+import CookieConsent from '../components/CookieConsent'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>My Cookie Website</title>
+      </Head>
+      <main style={{ padding: "2rem" }}>
+        <h1>Welcome to My Website</h1>
+        <p>This is a test site that uses cookies.</p>
+      </main>
+      <CookieConsent />
+    </>
+  )
+}
